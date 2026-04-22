a4a7a2a971fc8ff15f5146c5992c73de5f36014f
Darce-Me <discarnivora@gmail.com>
2026-04-22 20:27:07 -0300

    Inicialização das classes e funções

--- src/planejadorviagem/Main.java
+++ src/planejadorviagem/Main.java
@@ -1,7 +1,46 @@
 package planejadorviagem;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        menuInicial();
+
     }
+
+    public static void menuInicial() {
+        int escolha;
+        do {
+            String[] opcoes = {"Planejar", "Sair"};
+            escolha = JOptionPane.showOptionDialog(null, "Escolha: ", "Início", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
+            System.out.println(escolha);
+
+            verificacao(escolha);
+        }while(escolha == 0);
+    }
+
+    public static void verificacao(int escolha) {
+        if (escolha == 0) {
+            planejar();
+        } else {
+            JOptionPane.showMessageDialog(null, "Tenha um bom dia!");
+        }
+    }
+
+    public static void planejar() {
+
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+
+        String data = JOptionPane.showInputDialog("Data da viagem(dd-MM-yyyy): ");
+
+        String qtd = JOptionPane.showInputDialog("Quantos dias a viagem vai durar: ");
+        int qtdDias = Integer.parseInt(qtd);
+
+        String preco = JOptionPane.showInputDialog("Valor da diária: ");
+        double valor = Double.parseDouble(preco);
+
+    }
+
 }
--- src/planejadorviagem/Pessoa.java
+++ src/planejadorviagem/Pessoa.java
@@ -2,4 +2,48 @@ package planejadorviagem;
 
 public class Pessoa {
     
+    // Atributos
+    private String name;
+    private String date;
+    private int qtdDias;
+    private double valor;
+
+    // Constructor
+    public Pessoa() {
+        
+    }
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getQtdDias() {
+        return qtdDias;
+    }
+
+    public void setQtdDias(int qtdDias) {
+        this.qtdDias = qtdDias;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
 }
